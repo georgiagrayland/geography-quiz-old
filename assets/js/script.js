21d8545c3139aa487fae1b6bd281fc239db66098
georgiagrayland <georgiamayeva@live.co.uk>
2022-11-30 16:01:00 +0000

    add framework of basic difficulty question and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,60 @@ function selectQuestion(selectedDifficulty, questionNumber, correctScore, userna
             correctAnswer: "Washington D.C.",
             difficulty: "1",
             options: ["New York City", "Paris", "Toronto"],
-        }
+        },
+        {
+            question:"What is the longest river in the world?",
+            correctAnswer: "The River Nile",
+            difficulty: "1",
+            options: ["The Danube", "The Seine", "The Thames"],
+        },
+        {
+            question:"Where would you find the Great Wall?",
+            correctAnswer: "China",
+            difficulty: "1",
+            options: ["India", "Russia", "Sweden"],
+        },
+        {
+            question:"Which is the largest country on earth (by land mass)",
+            correctAnswer: "Russia",
+            difficulty: "1",
+            options: ["Spain", "Brazil", "Nigeria"],
+        },
+        {
+            question:"What is the tallest mountain in Europe?",
+            correctAnswer: "Mont Blanc",
+            difficulty: "1",
+            options: ["Ben Nevis", "Mount Everest", "Kilimanjaro"],
+        },
+        {
+            question:"Where are the Andes Mountains?",
+            correctAnswer: "South America",
+            difficulty: "1",
+            options: ["Europe", "Asia", "Australia"],
+        },
+        {
+            question:"What is the largest waterfall on Earth?",
+            correctAnswer: "Victoria Falls",
+            difficulty: "1",
+            options: ["Angel Falls", "Niagra Falls", "Sutherland Falls"],
+        },
+        {
+            question:"Which is the largest ocean on Earth?",
+            correctAnswer: "Pacific Ocean",
+            difficulty: "1",
+            options: ["Atlantic Ocean", "Antarctic Ocean", "Indian Ocean"],
+        },
+        {
+            question:"Where would you find Kangaroos in the wild?",
+            correctAnswer: "Australia",
+            difficulty: "1",
+            options: ["Pakistan", "South Africa", "Argentina"],
+        },
+        {
+            question:"Which is the poorest Country?",
+            correctAnswer: "Sudan",
+            difficulty: "1",
+            options: ["United Kingdom", "Canada", "Brazil"],
+        },
     ]
 }
