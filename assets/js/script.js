a76763e702057dbe02a4da497ef7123a02ff50ed
georgiagrayland <georgiamayeva@live.co.uk>
2022-11-30 15:48:46 +0000

    add initial structure for questions function in js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+
+
+//this is where the questions are returned correct or incorrect 
+function selectQuestion(selectedDifficulty, questionNumber, correctScore, username) {
+    let questions = [
+        {
+            question:"What is the Capital City of the USA?",
+            correctAnswer: "Washington D.C.",
+            difficulty: "1",
+            options: ["New York City", "Paris", "Toronto"],
+        }
+    ]
+}
